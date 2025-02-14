3e06202d09dbf5971d132d8ced982db23c5e97f6
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-02-14 10:45:24 -0300

    Case sensitive php

---
+++ conceitos_basicos/case_sensitive.php
@@ -0,0 +1,13 @@
+<?php
+    // Não é case sensitive
+    echo "teste <br>";
+    EchO "testando 2 <br>";
+    ECHO "testando 3 <br>";
+
+    // Variáveis são case sensitives = há diferenças entre maiusc. e minus.
+    $nome = 'Luis';
+    $NOME = 'José';
+    echo $nome;
+    echo "<br>";
+    echo $NOME;
+?>
