deb21bd5e68ef3b3e72d13de0687527627ae5d6c
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-02-14 10:52:57 -0300

    Instruções e delimitadores

---
+++ conceitos_basicos/instrucoes.php
@@ -0,0 +1,10 @@
+<?php 
+    // Instruções em php
+    // Delimitadores de instruções: ; e {}
+    echo 'Uma linha <br>';
+    echo 'Outra linha <br>';
+
+    if (10 > 2) {
+        echo 'Dentro do if';
+    }
+?>
