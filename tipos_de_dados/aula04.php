348ec756abd7c85d4e46e4db02091ce15939df5c
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-02-18 21:48:50 -0300

    Checando se um número é float

---
+++ tipos_de_dados/aula04.php
@@ -0,0 +1,12 @@
+<?php
+    $dado1 = 'teste';
+    $dado2 = 12.90;
+
+    if (is_float($dado1)) {
+        echo 'É float! <br>';
+    }
+    
+    if (is_float($dado2)) {
+        echo 'É float! <br>';
+    }
+?>
