0f58ef95b9e2af0c07942d448762b306cf7f2eed
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-02-20 08:59:15 -0300

    Básico de arrays

---
+++ tipos_de_dados/aula09.php
@@ -0,0 +1,17 @@
+<?php 
+    // Array
+    // print_r() -> Serve para a impressão de arrays.
+    $array = [1, 2, 3];
+    print_r($array);
+
+    echo "<br>";
+
+    
+    echo $array[0];
+    $array2 = ['José', 19, true];
+
+    echo '<br>';
+    print_r($array2);
+    echo '<br>';
+    print_r($array2[0]);
+?>
