e73873987f6b085022c7ef20f6052ce570c784a8
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-02-20 08:33:16 -0300

    Tipo de dado boolean

---
+++ tipos_de_dados/aula07.php
@@ -0,0 +1,15 @@
+<?php 
+    // Tipo boolean
+    echo true;
+    echo "<br>";
+    echo false;
+    echo "<br>";
+
+    if (true) {
+        echo 'Verdadeiro <br>';
+    }
+
+    if (10 > 2) {
+        echo 'Verdadeiro <br>';
+    }
+?>
