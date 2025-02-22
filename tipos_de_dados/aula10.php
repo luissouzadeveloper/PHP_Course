9cb099e3db32ec7861e56acf76c6a689570aa8e1
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-02-21 21:20:19 -0300

    Array associativo

---
+++ tipos_de_dados/aula10.php
@@ -0,0 +1,15 @@
+<?php 
+    // Array associativo
+
+    $carro = ['marca' => 'Chevrolet', 'cor' => 'Preto', 'portas' => 4];
+
+    echo $carro['marca'];
+    echo '<br>';
+    print_r($carro);
+    echo '<br>';
+    echo $carro['cor'];
+
+    $arrayAssoc = ['chave' => 'valor', 'bool' => true];
+    echo '<br>';
+    print_r($arrayAssoc);
+?>
