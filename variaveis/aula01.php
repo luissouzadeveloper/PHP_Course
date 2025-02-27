e253c92fe3b4dff8f58d8e42ce7e397dc6d109b4
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-02-27 08:58:05 -0300

    Declarando variáveis

---
+++ variaveis/aula01.php
@@ -0,0 +1,17 @@
+<?php 
+    // Declarando variáveis
+    $nome = 'Ana';
+    echo "$nome <br>";
+
+    $numero = 15.3;
+    echo "$numero <br>";
+
+    $aberto = true;
+    echo "$aberto <br>"; // Retorna 1
+
+    $_texto = 'Algum texto';
+    echo $_texto . '<br>';
+
+    $salarioBase = 1412.50; // <- Camel case
+    echo $salarioBase;
+?>
