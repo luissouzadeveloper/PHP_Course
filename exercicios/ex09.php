58dca9047d59b91cf9eeec14937e5082563b1858
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-03-05 11:52:44 -0300

    Exercício 09

---
+++ exercicios/ex09.php
@@ -0,0 +1,14 @@
+<?php 
+    $num = 10; // int
+    $numFloat = 1.6; // float
+    $texto = 'Hello World'; //string
+    $verdade = true; // bool
+    $pessoa = ['José', 80, 1.85]; // array
+
+    echo "$num <br>";
+    echo "$numFloat <br>";
+    echo "$texto <br>";
+    echo "$verdade <br>";
+    print_r($pessoa);
+
+?>
