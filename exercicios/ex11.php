74166b555a664e5bca118f958c5f110dc58ecac2
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-03-13 09:23:15 -0300

    Exercício 11

---
+++ exercicios/ex11.php
@@ -0,0 +1,16 @@
+<?php 
+    /*
+    Escreva um algoritmo para somar dois números e multiplicar o resultado pelo primeiro número.
+    */
+
+    $numero1 = 10;
+    $numero2 = 6;
+    $soma = $numero1 + $numero2;
+
+    echo $soma;
+    echo '<br>';
+
+    $multiplica = $numero1 * $soma;
+
+    echo $multiplica;
+?>
