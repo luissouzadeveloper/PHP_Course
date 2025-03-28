e3bdca65ebd22aa8b78447afa39886bc3000274e
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-03-27 23:29:49 -0300

    Ordem dos operadores

---
+++ expressoes_operadores/aula01.php
@@ -0,0 +1,15 @@
+<?php 
+    // Operadores
+    echo 3 + 5 * 3 . '<br>';
+    echo (3 + 5) * 3 . '<br>';
+
+    $n1 = 3;
+    $n2 = 10;
+    $n3 = 8;
+
+    echo $n1 - $n3 * $n2 . '<br>';
+
+    $operacao = $n1 + $n2 / $n3;
+
+    echo $operacao;
+?>
