9709de22df1bae1d7bccf9257216ec4f441406a6
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-03-30 10:04:59 -0300

    Exercício 12

---
+++ exercicios/ex12.php
@@ -0,0 +1,14 @@
+<?php 
+    $n1 = 5;
+    $n2 = 2;
+
+    $subtracao = $n1 - $n2;
+    $soma = $n1 + $n2;
+    $divisao = $n1 / $n2;
+    $multi = $n1 * $n2;
+
+    echo "Soma: $soma <br>";
+    echo "Subtração: $subtracao <br>";
+    echo "Multiplicação: $multi <br>";
+    echo "Divisão: $divisao <br>";
+?>
