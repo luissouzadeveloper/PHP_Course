7d364014fbf84d1cea39b9361f56c738df6c8d2b
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-03-30 22:35:49 -0300

    Operadores aritméticos

---
+++ expressoes_operadores/aula03.php
@@ -0,0 +1,20 @@
+<?php 
+    // Operadores aritméticos
+    /*
+    + (soma)
+    - (subtração)
+    * (multiplicação)
+    / (divisão)
+    */
+    $soma = 1 + 1;
+    echo "$soma <br>";
+
+    $subtracao = 58 - 23;
+    echo "$subtracao <br>";
+
+    $multiplicacao = 5 * 135;
+    echo "$multiplicacao <br>";
+
+    $divisao = 240 / 5;
+    echo "$divisao <br>";
+?>
