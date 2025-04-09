90d897f34a1180a86099256890d2784200d44742
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-04-08 23:21:50 -0300

    Exercício 16

---
+++ exercicios/ex16.php
@@ -0,0 +1,9 @@
+<?php 
+    $saudacao = 'Olá';
+    $nome = 'José';
+    $sobrenome = 'Silva';
+
+    $frase = $saudacao . ' Tudo bem? ' . 'Bem vindo ' . $nome . ' '. $sobrenome;
+
+    echo $frase;
+?>
