813b6582ba1293e8c7ffeae4c80c342c3b111dfd
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-04-14 10:07:01 -0300

    Operador de incremento e decremento

---
+++ expressoes_operadores/aula07.php
@@ -0,0 +1,20 @@
+<?php 
+    // Operador de incremento e decremento
+    $x = 0;
+    $y = 0;
+
+    $x++;
+    $y = $y + 1;
+
+    echo "X: $x <br>";
+    echo "Y: $y <br>";
+
+    $m = 15;
+    $n = 15;
+
+    $m--;
+    $n = $n - 1;
+
+    echo "M: $m <br>";
+    echo "N: $n <br>";
+?>
