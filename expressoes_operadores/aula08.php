a52ea64e57c019783baedd5591c7c58699240e6a
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-04-14 10:27:05 -0300

    Operador de igualdade

---
+++ expressoes_operadores/aula08.php
@@ -0,0 +1,30 @@
+<?php 
+    // Operador de igualdade
+    $n1 = 5;
+    $n2 = 5;
+
+    if ($n1 == $n2) {
+        echo "Verdadeiro <br>";
+    } else {
+        echo "Falso <br>";
+    }
+
+    $a = 15;
+    $b = 10;
+    $c = 15;
+
+    if ($a == $b) {
+        echo "Esta comparação é verdadeira 1";
+    }
+
+    if ($a == $c) {
+        echo "Esta comparação é verdadeira 2 <br>";
+    }
+
+    $name = "Luis";
+    $nameSys = "Luis";
+
+    if ($name == $nameSys) {
+        echo "O nome corresponde <br>";
+    }
+?>
