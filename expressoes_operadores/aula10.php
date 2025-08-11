c7225eb368d5a5157b072ae0d093a1e9de1e1aa3
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-11 10:02:12 -0300

    Operador de diferença

---
+++ expressoes_operadores/aula10.php
@@ -0,0 +1,14 @@
+<?php 
+    // Operador de diferença
+    $valor1 = 5;
+    $valor2 = 10;
+
+    if ($valor1 != $valor2) {
+        echo "São diferentes";
+    }
+
+    // São iguais
+    if ($valor2 != 10) {
+        echo "São iguais";
+    }
+?>
