5a15e5ab8487586ef7a444cc8c10bb6128988869
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-11 10:04:07 -0300

    Exercício 18

---
+++ exercicios/ex18.php
@@ -0,0 +1,20 @@
+<?php 
+    $n1 = 5;
+    $n2 = 3;
+
+    if ($n1 == $n2) {
+        echo "Os valores não são iguais <br>";
+    }
+
+    if ($n1 != $n2) {
+        echo "São diferentes <br>";
+    }
+
+    if ($n1 === $n2) {
+        echo "Não são iguais, mas são do mesmo tipo <br>";
+    }
+
+    if ($n1 !== $n2) {
+        echo "São diferentes, mas são do mesmo tipo <br>";
+    }
+?>
