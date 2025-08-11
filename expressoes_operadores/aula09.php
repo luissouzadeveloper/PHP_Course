c1ab42a173e4319eb605f6e7696087d50d52b3f1
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-11 10:01:36 -0300

    Operador de identidade

---
+++ expressoes_operadores/aula09.php
@@ -0,0 +1,13 @@
+<?php 
+    // Operador de identidade
+
+    if (5 === 5) {
+        // Retorna true, são de mesmo tipo e valor!
+        echo "São do mesmo tipo e valor!";
+    }
+
+    if (5 === '5') {
+        // São o mesmo valor, mas não o mesmo tipo!
+        echo "Não são do mesmo tipo!";
+    }
+?>
