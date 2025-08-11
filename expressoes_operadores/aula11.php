13f37e0a7d6b9fd5c4f0649c8bd3f687cfdd4743
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-11 10:03:00 -0300

    Operador não idêntico

---
+++ expressoes_operadores/aula11.php
@@ -0,0 +1,13 @@
+<?php 
+    // Operador não idêntico
+    $v1 = 1;
+    $v2 = '1';
+
+    if ($v1 !== $v2) {
+        echo "São diferentes <br>";
+    }
+
+    if (2 !== 2) {
+        echo "Não são diferentes";
+    }
+?>
