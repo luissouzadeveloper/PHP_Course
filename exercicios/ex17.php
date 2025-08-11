6050475788ebe4f9d5346cabce4c5f74bac52176
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-11 09:01:49 -0300

    Exercício 17

---
+++ exercicios/ex17.php
@@ -0,0 +1,13 @@
+<?php 
+    $n1 = 4;
+    $n2 = 5;
+    $n3 = 4;
+
+    if ($n1 == $n2) {
+        echo "n1 é igual a n2 <br>";
+    }
+
+    if ($n1 == $n3) {
+        echo "n1 é igual a n3 <br>";
+    }
+?>
