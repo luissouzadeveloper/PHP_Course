252cd30fa8c780f9ecf01d9f778f8b9262a708e3
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-12 08:57:01 -0300

    Operador de maior e maior ou igual

---
+++ expressoes_operadores/aula12.php
@@ -0,0 +1,19 @@
+<?php 
+    // Operador de maior e maior ou igual
+    $v1 = 4;
+    $v2 = 8;
+    $v3 = 20;
+    $v4 = 20;
+
+    if ($v1 > $v2) {
+        echo "É menor <br>";
+    }
+
+    if ($v3 > $v2) {
+        echo "É maior <br>";
+    }
+
+    if ($v3 >= $v4) {
+        echo "Não é maior mas é igual";
+    }
+?>
