957a45e93a08e6b27ec59299b050b84d515050da
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-13 09:17:41 -0300

    Operador menor e menor ou igual

---
+++ expressoes_operadores/aula13.php
@@ -0,0 +1,17 @@
+<?php 
+    // Operador menor e menor ou igual
+    if (4 < 17) {
+        echo "4 é menor que 17 <br>";
+    }
+
+    $valor1 = 5;
+    $valor2 = 8;
+
+    if ($valor2 < $valor1) {
+        echo "Valor 2 não é menor que valor 1 <br>";
+    }
+
+    if (17 <= 17) {
+        echo "Não é menor, mas é igual";
+    }
+?>
