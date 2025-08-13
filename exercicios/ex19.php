b92fce7235ffa8c1f5998eeb5be775675e66f972
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-13 09:18:50 -0300

    Exercício 19

---
+++ exercicios/ex19.php
@@ -0,0 +1,13 @@
+<?php 
+    if (15 > 5 && 'João' === 'João') {
+        echo "Entrou no if 1 <br>";
+    }
+
+    if ("teste" > 5 && 1) {
+        echo "Não entra no if 2 <br>";
+    }
+
+    if (2 == 3 && 5 >= 3) {
+        echo "Não entra no if 3 <br>";
+    }
+?>
