9471203d38bfe4ca65b6f19030f160f568ce6760
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-14 10:05:27 -0300

    Operador NOT

---
+++ expressoes_operadores/aula16.php
@@ -0,0 +1,12 @@
+<?php 
+    // Operador NOT
+    if (!(5 > 2)) {
+        // true -> false
+        echo "false <br>";
+    }
+
+    if (!(3 <= 1)) {
+        // false -> true
+        echo "true";
+    }
+?>
