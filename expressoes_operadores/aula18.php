140b13feac5f00cd36fe26c7ceda715597889261
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-14 10:28:21 -0300

    Operador de atribuição

---
+++ expressoes_operadores/aula18.php
@@ -0,0 +1,16 @@
+<?php 
+    // Operador de atribuição
+    $n = 5;
+    // $n = $n + 10;
+    $n += 10;
+    echo "$n <br>";
+
+    $n -= 10;
+    echo "$n <br>";
+
+    $n *= 10;
+    echo "$n <br>";
+
+    $n /= 10;
+    echo "$n <br>";
+?>
