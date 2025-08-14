2ef1fafabe38a019fb6a152b56d9b35866360ff9
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-14 09:49:00 -0300

    Operador OR

---
+++ expressoes_operadores/aula15.php
@@ -0,0 +1,17 @@
+<?php 
+    // Operador lógico OR
+    if (5 > 1 || 6 < 12) {
+        // true
+        echo "true <br>";
+    }
+
+    if (6 > 13 || 5 < 8) {
+        // true 
+        echo "true <br>";
+    }
+
+    if (8 > 10 || 3 < 2) {
+        // false 
+        echo "false";
+    }
+?>
