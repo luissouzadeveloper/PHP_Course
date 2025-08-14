682fdb957effd59e73aea50e61691dc27aa5cb20
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-14 10:14:04 -0300

    Exercício 21

---
+++ exercicios/ex21.php
@@ -0,0 +1,13 @@
+<?php 
+    $text = (int) 'testando';
+    echo "$text <br>";
+
+    $float = (int) '5.75';
+    echo "$float <br>";
+
+    $true = (int) true;
+    echo "$true <br>";
+
+    $array = (int) [1, 2, 3];
+    echo "$array <br>";
+?>
