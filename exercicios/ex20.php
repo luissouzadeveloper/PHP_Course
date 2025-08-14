455092b75c19f81013fb97c0c07217ac1aeb9793
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-14 09:48:08 -0300

    Exercício 20

---
+++ exercicios/ex20.php
@@ -0,0 +1,16 @@
+<?php 
+    if (12 < 5 || "João" === "João") {
+        // true
+        echo "true <br>";
+    }
+
+    if (1 > 5 || 1) {
+        // true
+        echo "true <br>";
+    }
+
+    if (20 === '20' && 51 >= 31) {
+        // false
+        echo "false";
+    }
+?>
