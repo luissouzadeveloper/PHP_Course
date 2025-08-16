156def5678d4dcb973d238e9a5a3353ec028f599
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-16 19:24:49 -0300

    Exercício 23

---
+++ exercicios/ex23.php
@@ -0,0 +1,18 @@
+<?php 
+    if (5 > 2) {
+        echo "Sim, 5 é maior que 2 <br>";
+    }
+
+    if ('Matheus' !== 'Pedro') {
+        echo "Sim, é diferente <br>";
+    }
+
+    if (12 <= 11) {
+        echo "Não, 12 não é menor e nem igual a 11";
+    }
+
+    $idade = 18;
+    if ($idade >= 18) {
+        echo "Já é de maior!";
+    }
+?>
