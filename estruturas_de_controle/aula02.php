985d2063e64e6f0d37531400f07c042e76394849
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-16 19:31:14 -0300

    Estrutura if e else

---
+++ estruturas_de_controle/aula02.php
@@ -0,0 +1,12 @@
+<?php 
+    // if e else
+    $numero1 = 5;
+    $numero2 = 8;
+
+    if ($numero1 > $numero2) {
+        echo "$numero1 é maior que $numero2";
+    } else {
+        echo "$numero1 é menor que $numero2";
+    }
+    
+?>
