c3e490af96d8b33d4c27fe9a46efdf86f2335be2
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-18 00:33:22 -0300

    Exercício 27

---
+++ exercicios/ex27.php
@@ -0,0 +1,8 @@
+<?php 
+    $peso = 79.15;
+    if ($peso >= 80) {
+        echo "O peso é de $peso kg, está muito pesado";
+    } else {
+        echo "O peso é de $peso kg, está dentro do limite";
+    }
+?>
