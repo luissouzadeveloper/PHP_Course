85ba9fba10ba1facc0a6c63fd360a059a971e05a
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-20 08:54:53 -0300

    Estrutura switch

---
+++ estruturas_de_controle/aula05.php
@@ -0,0 +1,24 @@
+<?php 
+    // Estrutura switch
+
+    $opcao = 1;
+
+    switch ($opcao) {
+        
+        case 0:
+            echo "0";
+            break;
+
+        case 1:
+            echo "1";
+            break;
+
+        case 2:
+            echo "2";
+            break;
+        
+        default:
+            echo "Inválido!";
+            break;
+    }
+?>
