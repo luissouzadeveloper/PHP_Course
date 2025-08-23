0d630346e8a996101e056e99968564b28057a84a
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-23 11:32:10 -0300

    do while

---
+++ estrutura_de_repetição/aula05.php
@@ -0,0 +1,17 @@
+<?php 
+    // do while
+    $i = 1;
+
+    do {
+        echo "Elemento: $i <br>";
+        $i++;
+    } while ($i <= 10);
+
+    echo "<br>";
+
+    $j = 10;
+    do {
+        echo "Elemento: $j <br>";
+        $j--;
+    } while ($j > 0);
+?>
