cd739698b48fd9530614f86f39a7185b29a05aee
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-25 11:09:34 -0300

    Include

---
+++ inclusao_de_codigos/include/aula01.php
@@ -0,0 +1,10 @@
+<?php 
+    include "teste.php";
+?>
+
+<p>
+    Include em PHP
+</p>
+<p>
+    A variável teste é <?php echo $teste; ?>
+</p>
---
+++ inclusao_de_codigos/include/teste.php
@@ -0,0 +1,5 @@
+<?php 
+    $teste = 25;
+?>
+
+<h1>Olá, Mundo!</h1>
