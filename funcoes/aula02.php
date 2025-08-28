4d098afbf989b142e7594d885c40aaf1a55d98a4
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-28 11:03:49 -0300

    Invocando função

---
+++ funcoes/aula02.php
@@ -0,0 +1,15 @@
+<?php 
+    // Chamando funções
+    function soma() {
+        $n1 = 7;
+        $n2 = 9;
+        $soma = $n1 + $n2;
+        echo $soma . '<br>';
+    }
+
+    // Invoca
+    soma();
+    soma();
+
+    echo strtoupper('teste');
+?>
