534c9338a80535a0a8c8b31180368db851c8e796
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-08-28 11:04:56 -0300

    Exercício 35

---
+++ exercicios/ex35.php
@@ -0,0 +1,13 @@
+<?php 
+    function multiplica() {
+        $n1 = 5;
+        $n2 = 2;
+        $n3 = 1.8;
+
+        $multiplica = $n1 * $n2 * $n3;
+
+        echo $multiplica . '<br>';
+    }
+
+    multiplica();
+?>
