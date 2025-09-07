ab85ac603ab8e4dc0719b969aed48a06eeee9c1c
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-09-07 11:06:14 -0300

    Depuração com print_r e var_dump

---
+++ funcoes/aula09.php
@@ -0,0 +1,15 @@
+<?php 
+    $array = [
+        'Olá',
+        34,
+        12.98,
+        true,
+        ['Zé', 78]
+    ];
+
+    print_r($array);
+
+    echo '<br>';
+
+    var_dump($array);
+?>
