a71a00625a19bcc987080d70e8789336fdac04f2
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-09-16 11:39:44 -0300

    Valores de escapes

---
+++ strings/aula02.php
@@ -0,0 +1,15 @@
+<?php 
+    header("Content-type: text/plain");
+
+    // Pular linha
+    echo "Primeira linha \n Segunda linha \n";
+
+    // Tab
+    echo "Texto com tab \t nessa linha \n";
+
+    // Barra invertida
+    echo "Barra invertida \\ \n";
+
+    // Dólar
+    echo "Sinal de dólar \$50"
+?>
