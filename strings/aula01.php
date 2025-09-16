1c83904bd2d59d7619e6ee9cae4f19178b460793
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-09-16 11:30:34 -0300

    Interpolação de strings

---
+++ strings/aula01.php
@@ -0,0 +1,10 @@
+<?php 
+    // Interpolação de variáveis
+
+    $nome = 'José';
+    $idade = 18;
+    $profissao = 'programador';
+
+    echo "Eu sou $nome, tenho $idade anos e atuo como $profissao <br>";
+    echo "Eu sou {$nome}, tenho {$idade} anos e atuo como {$profissao}";
+?>
