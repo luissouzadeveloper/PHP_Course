5c0eea5475ed07e1594b0fd7878272856cd5f5e9
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-09-26 11:28:43 -0300

    Função printf

---
+++ strings/aula04.php
@@ -0,0 +1,14 @@
+<?php 
+    // Função printf
+    $nome = 'Zé';
+    // %s -> strings
+    printf("O nome é %s <br>", $nome);
+
+    // $d -> int
+    $num = 20;
+    printf("O número é %d <br>", $num);
+
+    // $f -> float
+    $peso = 64.67;
+    printf("O peso é %.1f <br>", $peso);
+?>
