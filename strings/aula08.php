af012964cdceb471c6f87d950ef10e22a9d58740
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-09-29 11:03:36 -0300

    Alterando case das strings

---
+++ strings/aula08.php
@@ -0,0 +1,11 @@
+<?php 
+    // Alterando case das strings
+    $string1 = 'Uma frase';
+    $string2 = 'Outra frase';
+
+    $maisc = strtoupper($string1);
+    $minus = strtolower($string2);
+
+    echo "Maiúsculo: $maisc <br>";
+    echo "Minúsculo: $minus <br>";
+?>
