8d11424885ca74424c2bf7578464b70b66ec42ef
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-09-29 11:11:24 -0300

    Alterando o case da primeira letra

---
+++ strings/aula09.php
@@ -0,0 +1,13 @@
+<?php 
+    // Alterando o case da primeira letra
+    $frase = "uma frase qualquer";
+    $frase2 = "outra frase qualquer";
+
+    // Primeira letra em maiúscula
+    echo ucfirst($frase) . '<br>';
+
+    // Todas as primeiras letras em maiúsculas
+    echo ucwords($frase2) . '<br>';
+
+
+?>
