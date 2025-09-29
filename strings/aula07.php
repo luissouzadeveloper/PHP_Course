1741211e9fffe6458edeac584773ee6b388e6ecc
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-09-29 10:06:25 -0300

    Limpando strings

---
+++ strings/aula07.php
@@ -0,0 +1,14 @@
+<?php 
+    // Limpando strings
+    $str1 = '    João    ';
+    echo "Esta é string 1: $str1. <br>";
+
+    $strLimpa = trim($str1);
+
+    echo "Esta é string 1: $strLimpa. <br>";
+
+    $strLimpa2 = rtrim($str1);
+
+    echo "Esta é string 1: $strLimpa2. <br>";
+
+?>
