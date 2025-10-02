47761ca7c4ee162f2a6aeaf70d35c4940621aa22
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-10-02 10:28:28 -0300

    Revertendo strings

---
+++ strings/aula12.php
@@ -0,0 +1,15 @@
+<?php 
+    // Revertendo strings
+    $palavra = 'Carro';
+
+    $palavraInvertida = strrev($palavra);
+
+    echo "$palavra <br>";
+    echo "$palavraInvertida <br>";
+
+    $frase = 'O programador pensa demais';
+    $fraseInvertida = strrev($frase);
+    
+    echo "$frase <br>";
+    echo "$fraseInvertida <br>";
+?>
